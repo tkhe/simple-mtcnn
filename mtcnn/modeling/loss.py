b1778ee18b41d75c358141e2891f738c3bd470bf
kazuha <violin1229@163.com>
2019-03-06 17:14:58 +0800

    add focal loss

--- mtcnn/modeling/loss.py
+++ mtcnn/modeling/loss.py
@@ -1,10 +1,37 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+from mtcnn.config import cfg
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha, gamma):
+        self.alpha = torch.Tensor([1 - alpha, alpha])
+        self.gamma = gamma
+
+    def forward(self, input, target):
+        input = torch.squeeze(input)
+        target = torch.squeeze(target)
+        target = target.view(-1, 1)
+
+        logpt = F.log_softmax(input, dim=1)
+        logpt = logpt.gather(1, target)
+        logpt = logpt.view(-1)
+        pt = logpt.exp()
+        if target.is_cuda:
+            self.alpha = self.alpha.cuda()
+        at = self.alpha.gather(0, target.view(-1))
+        logpt = logpt * at
+        loss = -1 * (1 - pt) ** self.gamma * logpt
+        return loss.mean()
 
 
 class Loss(object):
     def __init__(self):
-        self.loss_cls = nn.CrossEntropyLoss()
+        if cfg.MODEL.FOCAL_LOSS_ON:
+            self.loss_cls = FocalLoss(cfg.FOCAL_LOSS.ALPHA, cfg.FOCAL_LOSS.GAMMA)
+        else:
+            self.loss_cls = nn.CrossEntropyLoss()
         self.loss_reg = nn.SmoothL1Loss()
 
     def __call__(self, cls, reg, label, target):
